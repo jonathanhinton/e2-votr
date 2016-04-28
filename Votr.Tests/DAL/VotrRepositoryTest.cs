a76d5ae084ef35944a6ccebb0ad28b8e4cda08ea
Jonathan Hinton <hintonjonathan@gmail.com>
2016-04-27 21:29:01 -0500

    interface and refactor

--- Votr.Tests/DAL/VotrRepositoryTest.cs
+++ Votr.Tests/DAL/VotrRepositoryTest.cs
@@ -3,12 +3,28 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Votr.DAL;
 using System.Collections.Generic;
 using Votr.Models;
+using Moq;
+using System.Linq;
+using System.Data.Entity;
 
 namespace Votr.Tests.DAL
 {
     [TestClass]
     public class VotrRepositoryTest
     {
+        //these two methods will run before and after every single test in order to set up a mock database in which to test methods, and then remove info so that there is no residual data that gets in the database.
+        [TestInitialize]
+        public void Initialize()
+        {
+
+        }
+
+        [TestCleanup]
+        public void Cleanup()
+        {
+
+        }
+
         [TestMethod]
         public void RepoEnsureICanCreateInstance()
         {
@@ -31,8 +47,25 @@ namespace Votr.Tests.DAL
         [TestMethod]
         public void RepoEnsureThereAreNoPolls()
         {
+
+            //Mocking
+            List<Poll> datasource = new List<Poll>();
+            Mock<VotrContext> mock_context = new Mock<VotrContext>();
+            Mock<DbSet<Poll>> mock_polls_table = new Mock<DbSet<Poll>>(); // Fake Polls Table
+
             // Arrange 
-            VotrRepository repo = new VotrRepository();
+            VotrRepository repo = new VotrRepository(mock_context.Object); //Injects mocked (fake) VotrContext
+            //IQueryable<Poll> data = datasource.AsQueryable(); //turn List<Poll> into something we can query with System.Linq | must be using System.Linq for this to work.
+            var data = datasource.AsQueryable(); //<--This is another option. and Casting
+
+            // telling our fake DBset to user our datasource like something Queryable, all four lines are necessary
+            mock_polls_table.As<IQueryable<Poll>>().Setup(m => m.GetEnumerator()).Returns(data.GetEnumerator());
+            mock_polls_table.As<IQueryable<Poll>>().Setup(m => m.ElementType).Returns(data.ElementType);
+            mock_polls_table.As<IQueryable<Poll>>().Setup(m => m.Expression).Returns(data.Expression);
+            mock_polls_table.As<IQueryable<Poll>>().Setup(m => m.Provider).Returns(data.Provider);
+
+            // Tell our mocked VotrContext to use our fully mocked datasource. (List<Poll>)
+            mock_context.Setup(m => m.Polls).Returns(mock_polls_table.Object);
 
             // Act
             List<Poll> list_of_polls = repo.GetPolls();
--- Votr/DAL/VotrRepository.cs
+++ Votr/DAL/VotrRepository.cs
@@ -10,10 +10,17 @@ namespace Votr.DAL
     {
         public VotrContext context { get; set; }
 
+        //this constructor will initialize if no argument is passed in.
         public VotrRepository()
         {
             // We need an instance of a Context
             context = new VotrContext();
+        }
+
+        //this provides a way for us to pass a context into the votr repository so that we don't have to rely on a database for testing. This constructor will initialize if we pass in an argument.
+        public VotrRepository(VotrContext _context)
+        {
+            context = _context;
         }
 
         public int GetPollCount()

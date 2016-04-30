2ee524e22c2eb80b2b99b3d52fb7b2fb136ada54
Jonathan Hinton <hintonjonathan@gmail.com>
2016-04-30 11:39:07 -0500

    write methods and exception class for removing poll

--- Votr/DAL/VotrRepository.cs
+++ Votr/DAL/VotrRepository.cs
@@ -39,6 +39,35 @@ namespace Votr.DAL
             context.Polls.Add(new_poll);
             context.SaveChanges();
         }
+
+        public Poll GetPoll(int _poll_id)
+        {
+
+            //return context.Polls.Find(_poll_id); // requires explilic mocking of the DbSet.Find Method
+            Poll poll;
+
+            try
+            {
+                poll = context.Polls.First(i => i.PollId == _poll_id);
+            } catch(Exception)
+            {
+                throw new NotFoundException();
+            }
+            return poll; //using this overloaded method allows us to leverage to properties of LINQ, ConnectMocksToDatastore made this method possible
+        }
+
+        public void RemovePoll(int _poll_id)
+        {
+            Poll some_poll = context.Polls.First(i => i.PollId == _poll_id);
+
+            context.Polls.Remove(some_poll);
+            context.SaveChanges();
+        }
+
+        public Poll GetPollOrNull(int _poll_id)
+        {
+            return context.Polls.FirstOrDefault(i => i.PollId == _poll_id);
+        }
         // Create a Poll
 
         // Delete a Poll
